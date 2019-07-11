9edb6cd40f1139b55817611cd8274aceb6b33ea2
fcakyon <akyon@ee.bilkent.edu.tr>
2019-07-11 12:53:50 +0300

    fixed popup panel big opening closing bug caused by multitouch

--- Assets/Scripts/AppUI Scipts/CanvasObjects/Decor/DecorPopupPanelBigController.cs
+++ Assets/Scripts/AppUI Scipts/CanvasObjects/Decor/DecorPopupPanelBigController.cs
@@ -10,16 +10,30 @@ public class DecorPopupPanelBigController : MonoBehaviour, ICanvasPanel
 
     public void ClosePanel()
     {
-        isPanelOpen = false;
+        StartCoroutine(ClosePanelCoroutine());
+    }
+
+    public IEnumerator ClosePanelCoroutine()
+    {
         cameraMaskAnimator = gameObject.GetComponent<Animator>();
         cameraMaskAnimator.Play("Popup Panel Big Out");
+        // to set ispanelopen to true only after the opening animaiton is finished
+        yield return new WaitForSeconds(0.01f);
+        isPanelOpen = false;
     }
 
     public void OpenPanel()
     {
-        isPanelOpen = true;
+        StartCoroutine(OpenPanelCoroutine());
+    }
+
+    public IEnumerator OpenPanelCoroutine()
+    {
         cameraMaskAnimator = gameObject.GetComponent<Animator>();
         cameraMaskAnimator.Play("Popup Panel Big In");
+        // to set ispanelopen to false only after the closing animaiton is finished
+        yield return new WaitForSeconds(0.01f);
+        isPanelOpen = true;
     }
 
 }
